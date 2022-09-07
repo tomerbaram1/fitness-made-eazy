dee0f54f61c4c66e14e746a5a76841ccc10df202
Sagi Barabi <sbarabi1@gmail.com>
2022-09-07 10:28:47 +0300

    added selectedProgramReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Home from "./features/Home/Home";
 import { Routes, Route } from "react-router-dom";
-
-
+import { Programs } from "./features/Programs/Programs";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route></Route>
+        <Route path="/Programs" element={<Programs></Programs>}></Route>
         <Route></Route>
         <Route></Route>
       </Routes>
