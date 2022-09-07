a3bf8f2700a7202fe5a978f3b3ccc0c5e4bb1707
Tomer <tomerbaram26@gmail.com>
2022-09-07 12:14:53 +0300

    added-learn-path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Home from "./features/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import { Programs } from "./features/Programs/Programs";
+import Learn from "./features/Learn/Learn";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/Programs" element={<Programs></Programs>}></Route>
-        <Route></Route>
+        <Route path="/Learn" element={<Learn></Learn>}></Route>
         <Route></Route>
       </Routes>
     </div>
--- src/features/Programs/Program.jsx
+++ src/features/Programs/Program.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { change, reset } from "../../reducers/SelectedProgramSlice";
 
 export const Program = (props) => {
+  const selectedProgram = useSelector((state) => state.selectedProgram.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="program-card">
       <div className="card-body">
         <img className="card-img" src={props.img}></img>
         <h2 className="card-title">{props.title}</h2>
         <span className="card-price">{props.price}</span>
-        <button className="btn">Learn</button>
+        <Link to="/Learn">
+          <button className="btn" onClick={props.handleClick}>
+            Learn
+          </button>
+        </Link>
         <button className="btn">Buy</button>
       </div>
     </div>
