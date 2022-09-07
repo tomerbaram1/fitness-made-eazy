102d4c08456fe3e15ff80e396aa779158dd905d1
Sagi Barabi <sbarabi1@gmail.com>
2022-09-07 10:31:33 +0300

    added selectedProgramReducer

--- src/features/Programs/Program.jsx
+++ src/features/Programs/Program.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { change, reset } from "../../reducers/SelectedProgramSlice";
 
 export const Program = (props) => {
-  const selectedProgram = useSelector((state) => state.selectedProgram.value);
-  const dispatch = useDispatch();
   return (
     <div className="program-card">
       <div className="card-body">
