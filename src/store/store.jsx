39ec25a3c42d96eeb40bdf7bcdbf0d7d54c30358
Tomer <tomerbaram26@gmail.com>
2022-09-08 10:31:31 +0300

    added cards

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import OpenLoginSlice from "../reducers/OpenLoginSlice";
+import OpenPaymentSlice from "../reducers/OpenLoginSlice";
 import OpenMyCartSlice from "../reducers/OpenMyCartSlice";
 import OpenRegisterSlice from "../reducers/OpenRegisterSlice";
 
@@ -8,6 +9,7 @@ import SelectedProgramSlice from "../reducers/SelectedProgramSlice";
 export const store = configureStore({
   reducer: {
     openLogin: OpenLoginSlice,
+    openPayment: OpenPaymentSlice,
     openRegister: OpenRegisterSlice,
     openMyCart: OpenMyCartSlice,
     selectedProgram: SelectedProgramSlice,
