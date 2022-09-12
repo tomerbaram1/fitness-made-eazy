4a86c876d23f3890189270275b4e27121d2e7732
Sagi Barabi <sbarabi1@gmail.com>
2022-09-12 10:19:52 +0300

    minor changes

--- src/core/About/about-main/Template.jsx
+++ src/core/About/about-main/Template.jsx
@@ -4,7 +4,7 @@ const Template = (props) => {
   return (
     <div className="valueBox">
       <img alt="about" src={props.img} />
-      <h3>{props.title}</h3>
+      <h3 style={{ color: "orange" }}>{props.title}</h3>
       <p>{props.par}</p>
     </div>
   );
