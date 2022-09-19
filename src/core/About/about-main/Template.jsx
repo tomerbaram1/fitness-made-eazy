b66b93ec5f3209c1d8107185fcd7db400ed718aa
Sagi Barabi <sbarabi1@gmail.com>
2022-09-19 11:53:33 +0300

    fixed netlify

--- src/core/About/about-main/Template.jsx
+++ src/core/About/about-main/Template.jsx
@@ -5,8 +5,6 @@ const Template = (props) => {
     <div className="valueBox">
       <img alt="about" src={props.img} />
 
-      <img alt="about" src={props.img} />
-
       <h3 style={{ color: "orange" }}>{props.title}</h3>
       <p>{props.par}</p>
     </div>
